300461e299a61e7e2424a46a9e869164a4050956
mina <mina214@sookmyung.ac.kr>
2024-07-15 01:08:33 +0900

    Feat: NPC의 움직임 추가

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -5,17 +5,20 @@ public class NPCController : MonoBehaviour
 {
     public DialogueManager dialogueManager;
     public string[] dialogueTexts; // 여러 개의 대화 텍스트를 담을 배열
+    private int currentDialogueIndex = 0; // 현재 대화 인덱스
+
+    private Animator animator; // Animator 컴포넌트 참조
 
     void Start()
     {
         dialogueManager = FindObjectOfType<DialogueManager>(); // 다이얼로그 매니저 참조
+        animator = GetComponent<Animator>(); // Animator 컴포넌트 가져오기
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // 플레이어와 충돌 시 다이얼로그 트리거
             dialogueManager.StartDialogue(dialogueTexts); // 대화 시작
         }
     }

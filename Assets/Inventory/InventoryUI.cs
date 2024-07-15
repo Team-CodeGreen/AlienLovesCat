cf3fa48da86a781f6c32593d4f832442b422714e
Mooni <88727410+moon0727@users.noreply.github.com>
2024-07-15 16:02:14 +0900

    Feat: Inventory script 작성

--- Assets/Inventory/InventoryUI.cs
+++ Assets/Inventory/InventoryUI.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryUI : MonoBehaviour
+public class InventoryUI : MonoBehaviour  //Canvas에 포함되는 Component
 {
-    public GameObject totalInventoryPanel;
-    bool activeTotalInventory = false;
+    public GameObject totalInventoryPanel;  //인벤토리 전체보기 UI
+    bool activeTotalInventory = false;  
 
     private void Start()
     {
-        totalInventoryPanel.SetActive(activeTotalInventory);
+        totalInventoryPanel.SetActive(activeTotalInventory); //비활성화로 시작
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I))  //I키가 눌렸을 경우 상태 전환
         {
             activeTotalInventory = !activeTotalInventory;
             totalInventoryPanel.SetActive(activeTotalInventory);

13bdf18be1efa8b59babce950781fe92e0416727
mina <mina214@sookmyung.ac.kr>
2024-07-17 18:10:55 +0900

    Fix: 화면 비율 수정
    NPC Controller에서 필요없는 변수가 있어서 주석처리함

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -5,7 +5,7 @@ public class NPCController : MonoBehaviour
 {
     public DialogueManager dialogueManager;
     public string[] dialogueTexts; // 여러 개의 대화 텍스트를 담을 배열
-    private int currentDialogueIndex = 0; // 현재 대화 인덱스
+    //private int currentDialogueIndex = 0; // 현재 대화 인덱스
 
     private Animator animator; // Animator 컴포넌트 참조
 

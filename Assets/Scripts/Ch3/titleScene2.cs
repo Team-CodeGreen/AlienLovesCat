62c980f42807750cc5d794501d0cba261fe95517
mina <mina214@sookmyung.ac.kr>
2024-08-12 02:56:00 +0900

    feat: font, 분리수거게임 타이틀, 씬연결

---
+++ Assets/Scripts/Ch3/titleScene2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleScene2 : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // 첫 번째 스테이지 씬 로드
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void OpenHelp()
+    {
+        // 도움말 씬 로드
+        SceneManager.LoadScene("HelpScene");
+    }
+}

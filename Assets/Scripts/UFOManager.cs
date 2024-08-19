ca2cc8cd1e1ac7cda4f4841893d65b8c36cf92ca
mina <mina214@sookmyung.ac.kr>
2024-08-20 04:33:52 +0900

    feat: 씬연결(ch3)

--- Assets/Scripts/UFOManager.cs
+++ Assets/Scripts/UFOManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UFOManager : MonoBehaviour
 {
@@ -9,7 +10,29 @@ public class UFOManager : MonoBehaviour
     public GameObject KeyImageSpace;
     public string nextScene;
 
-    private bool playerInRange = false; // 플레이어가 콜라이더 안에 있는지 추적하는 변수
+
+    private void Start()
+    {
+        UFOButton.SetActive(false); // 버튼을 초기 상태에서 비활성화
+
+        // 버튼의 OnClick 이벤트에 메서드를 연결
+        if (UFOButton != null)
+        {
+            Button buttonComponent = UFOButton.GetComponent<Button>();
+            if (buttonComponent != null)
+            {
+                buttonComponent.onClick.AddListener(OnUFOButtonClicked);
+            }
+            else
+            {
+                Debug.LogError("Button component missing on UFOButton.");
+            }
+        }
+        else
+        {
+            Debug.LogError("UFOButton reference not set.");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -17,7 +40,6 @@ public class UFOManager : MonoBehaviour
         {
             UFOButton.SetActive(true); // UFO 버튼을 활성화
             KeyImageSpace.SetActive(true); // KeyImageSpace를 활성화
-            playerInRange = true; // 플레이어가 범위 안에 있다고 표시
         }
     }
 
@@ -27,15 +49,20 @@ public class UFOManager : MonoBehaviour
         {
             UFOButton.SetActive(false); // UFO 버튼을 비활성화
             KeyImageSpace.SetActive(false); // KeyImageSpace를 비활성화
-            playerInRange = false; // 플레이어가 범위 밖으로 나갔다고 표시
         }
     }
 
     private void Update()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.Space)) // 플레이어가 범위 안에 있고 스페이스바를 누르면
+        if (Input.GetKeyDown(KeyCode.Space)) // 플레이어가 범위 안에 있고 스페이스바를 누르면
         {
             SceneManager.LoadScene(nextScene); // 씬 변경
         }
+    }
+
+    // 버튼이 눌렸을 때 호출될 메서드
+    private void OnUFOButtonClicked()
+    {
+            SceneManager.LoadScene(nextScene); // 씬 변경
     }
 }
